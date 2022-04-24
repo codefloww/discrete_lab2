a3fa8eda624e448357d0545191256760aea56825
codefloww <kryvpaul@gmail.com>
2022-04-24 13:30:43 +0300

    fixed problems with multiple users

--- my_client.py
+++ my_client.py
@@ -4,55 +4,65 @@ from datetime import datetime
 
 from encoding import Encrypting
 
-# server's IP address
-# if the server is not on this machine, 
-# put the private (network) IP address (e.g 192.168.1.2)
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 5002 # server's port
-separator_token = "<SEP>" # we will use this to separate the client name & message
-padding_token = "<PAD>"
 
-# initialize TCP socket
-s = socket.socket()
-print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
-# connect to the server
-s.connect((SERVER_HOST, SERVER_PORT))
-print("[+] Connected.")
+class Client:
+    def __init__(self, server_host: str = None, server_port: int = None, username: str = None) -> None:
+        # server's IP address
+        # if the server is not on this machine, 
+        # put the private (network) IP address (e.g 192.168.1.2)
+        self.SERVER_HOST = server_host or "127.0.0.1"
+        self.SERVER_PORT = server_port or 5002 # server's port
+        self.separator_token = "<SEP>" # we will use this to separate the client name & message
+        self.padding_token = "<PAD>"
+        # initialize TCP socket
+        self.s = socket.socket()
+        print(f"[*] Connecting to {self.SERVER_HOST}:{self.SERVER_PORT}...")
+        # connect to the server
+        self.s.connect((self.SERVER_HOST, self.SERVER_PORT))
+        print("[+] Connected.")
+    def run(self):
 
-server_public = s.recv(1024).decode()
-server_public = tuple(map(int,server_public[1:-1].split(', ')))
+        self.server_public = self.s.recv(1024).decode()
+        self.server_public = tuple(map(int,self.server_public[1:-1].split(', ')))
 
-client_public, client_private = Encrypting().get_keys()
+        self.client_public, self.client_private = Encrypting().get_keys()
 
-client_public_encoded = Encrypting().encrypt_message(str(client_public), server_public)
-s.send(str(client_public_encoded).encode())
-print(server_public)
-# prompt the client for a name
-name = input("Enter your name: ")
+        client_public_encoded = Encrypting().encrypt_message(str(self.client_public), self.server_public)
+        self.s.send(str(client_public_encoded).encode())
+        print(self.server_public)
+        # prompt the client for a name
+        name = input("Enter your name: ")
 
-def listen_for_messages():
-    while True:
-        message = Encrypting().decrypt_message(s.recv(1024).decode(), client_private)
-        print("\n" + message)
+        # make a thread that listens for messages to this client & print them
+        t = Thread(target=self.listen_for_messages)
+        # make the thread daemon so it ends whenever the main thread ends
+        t.daemon = True
+        # start the thread
+        t.start()
 
-# make a thread that listens for messages to this client & print them
-t = Thread(target=listen_for_messages)
-# make the thread daemon so it ends whenever the main thread ends
-t.daemon = True
-# start the thread
-t.start()
+        while True:
+            # input message we want to send to the server
+            to_send =  input()
+            # a way to exit the program
+            if to_send.lower() == 'q':
+                break
+            # add the datetime, name & the color of the sender
+            date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+            to_send = f"[{date_now}] {name}{self.separator_token}{to_send}"
+            # finally, send the message
+            self.s.send(Encrypting().encrypt_message(to_send, self.server_public).encode())
 
-while True:
-    # input message we want to send to the server
-    to_send =  input()
-    # a way to exit the program
-    if to_send.lower() == 'q':
-        break
-    # add the datetime, name & the color of the sender
-    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-    to_send = f"[{date_now}] {name}{separator_token}{to_send}"
-    # finally, send the message
-    s.send(Encrypting().encrypt_message(to_send, server_public).encode())
 
-# close the socket
-s.close()
+    def listen_for_messages(self):
+        while True:
+            message = Encrypting().decrypt_message(self.s.recv(1024).decode(), self.client_private)
+            print("\n" + message)
+
+    def close(self):
+        # close the socket
+        self.s.close()
+
+if __name__ == "__main__":
+    client = Client()
+    client.run()
+    client.close
--- my_server.py
+++ my_server.py
@@ -1,81 +1,92 @@
 import socket
 from threading import Thread
 from encoding import Encrypting
+class Server:
+    def __init__(self, port: int = None, host: str = None) -> None:
 
-# server's IP address
-SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 5002  # port we want to use
-separator_token = "<SEP>"  # we will use this to separate the client name & message
-padding_token = "<PAD>"
-# initialize list/set of all connected client's sockets
-client_sockets = set()
-client_keys = {}
-# create a TCP socket
+        # server's IP address
+        self.SERVER_HOST = host or "0.0.0.0"
+        self.SERVER_PORT = port or 5002  # port we want to use
+        self.separator_token = "<SEP>"  # we will use this to separate the client name & message
+        self.padding_token = "<PAD>"
+        # initialize list/set of all connected client's sockets
+        self.client_sockets = set()
+        self.client_keys = {}
+        # create a TCP socket
 
-s = socket.socket()
-# make the port as reusable port
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 128)
-# bind the socket to the address we specified
-s.bind((SERVER_HOST, SERVER_PORT))
-# listen for upcoming connections
-s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+        self.s = socket.socket()
+        # make the port as reusable port
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 128)
+        # bind the socket to the address we specified
+        self.s.bind((self.SERVER_HOST, self.SERVER_PORT))
+
+    def start(self):
+        # listen for upcoming connections
+        self.s.listen(5)
+        print(f"[*] Listening as {self.SERVER_HOST}:{self.SERVER_PORT}")
 # generates a new pair of keys for server
-server_public, server_private = Encrypting().get_keys()
+        self.server_public, self.server_private = Encrypting().get_keys()
 
 
-def listen_for_client(cs):
-    """
-    This function keep listening for a message from `cs` socket
-    Whenever a message is received, broadcast it to all other connected clients
-    """
-    while True:
-        try:
-            # keep listening for a message from `cs` socket
-            msg = Encrypting().decrypt_message(cs.recv(1024).decode(), server_private)
-        except Exception as e:
-            # client no longer connected
-            # remove it from the set
-            print(f"[!] Error: {e}")
-            client_sockets.remove(cs)
-        else:
-            # if we received a message, replace the <SEP>
-            # token with ": " for nice printing
-            msg = msg.replace(separator_token, ": ")
-        # iterate over all connected sockets
-        for client_socket in client_sockets:
-            if client_socket != cs:
-                # and send the message to all other clients
-                client_socket.send(
-                    Encrypting()
-                    .encrypt_message(msg, client_keys[str(client_socket)])
-                    .encode()
-                )
+        while True:
+            # we keep listening for new connections all the time
+            client_socket, client_address = self.s.accept()
+            print(f"[+] {client_address} connected.")
+            # add the new connected client to connected sockets
+            self.client_sockets.add(client_socket)
 
+            client_socket.send(str(self.server_public).encode())
+            client_public = client_socket.recv(1024).decode()
+            client_public = Encrypting().decrypt_message(client_public, self.server_private)
 
-while True:
-    # we keep listening for new connections all the time
-    client_socket, client_address = s.accept()
-    print(f"[+] {client_address} connected.")
-    # add the new connected client to connected sockets
-    client_sockets.add(client_socket)
+            self.client_keys[str(client_socket)] = tuple(
+                map(int, client_public.rstrip()[1:-1].split(", "))
+            )
+            # start a new thread that listens for each client's messages
+            t = Thread(target=self.listen_for_client, args=(client_socket,))
+            # make the thread daemon so it ends whenever the main thread ends
+            t.daemon = True
+            # start the thread
+            t.start()
 
-    client_socket.send(str(server_public).encode())
-    client_public = client_socket.recv(1024).decode()
-    client_public = Encrypting().decrypt_message(client_public, server_private)
+    def listen_for_client(self,cs):
+        """
+        This function keep listening for a message from `cs` socket
+        Whenever a message is received, broadcast it to all other connected clients
+        """
+        while True:
+            try:
+                # keep listening for a message from `cs` socket
+                msg = Encrypting().decrypt_message(cs.recv(1024).decode(), self.server_private)
+            except Exception as e:
+                # client no longer connected
+                # remove it from the set
+                print(f"[!] Error: {e}")
+                self.client_sockets.remove(cs)
+            else:
+                # if we received a message, replace the <SEP>
+                # token with ": " for nice printing
+                msg = msg.replace(self.separator_token, ": ")
+            # iterate over all connected sockets
+            for client_socket in self.client_sockets:
+                if client_socket != cs:
+                    # and send the message to all other clients
+                    client_socket.send(
+                        Encrypting()
+                        .encrypt_message(msg, self.client_keys[str(client_socket)])
+                        .encode()
+                    )
+    def close(self):
+        # close client sockets
+        for cs in self.client_sockets:
+            cs.close()
+        # close server socket
+        self.s.close()
 
-    client_keys[str(client_socket)] = tuple(
-        map(int, client_public.rstrip()[1:-1].split(", "))
-    )
-    # start a new thread that listens for each client's messages
-    t = Thread(target=listen_for_client, args=(client_socket,))
-    # make the thread daemon so it ends whenever the main thread ends
-    t.daemon = True
-    # start the thread
-    t.start()
-
-# close client sockets
-for cs in client_sockets:
-    cs.close()
-# close server socket
-s.close()
+if __name__ == "__main__":
+    # create a server object
+    server = Server(port=5002)
+    # start the server
+    server.start()
+    # close the server
+    server.close()
